f2802674d9045d9099d7b431a57dd62bb80e723a
Sky Yao <challenge.cool@gmail.com>
2024-04-14 22:22:02 +0800

    feat: add styles for button

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -5,9 +5,9 @@ const meta: Meta<typeof Button> = {
   title: 'Cmoponents/Button',
   component: Button,
   parameters: {
-    layout: 'centered',
+    layout: 'centered'
   },
-  tags: ['autodocs'],
+  tags: ['autodocs']
 };
 
 export default meta;
@@ -17,5 +17,25 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: 'Click me',
-  },
+    variant: 'solid',
+    size: 'md',
+    colorscheme: 'primary'
+  }
+};
+
+export const Outline: Story = {
+  args: {
+    children: 'Click me',
+    variant: 'outline',
+    size: 'md',
+    colortheme: 'primary'
+  }
+};
+
+export const Ghost: Story = {
+  args: {
+    children: 'Click me',
+    variant: 'ghost',
+    size: 'md'
+  }
 };
